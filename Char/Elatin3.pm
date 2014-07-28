2051cb4b1dacd0acc67dc88d5b0bdfa90bc57fc8
INABA Hitoshi <ina@cpan.org>
2014-07-28 16:27:59 +0000

    Import of INA/Char-Latin3-0.97 from CPAN.
    gitpan-cpan-distribution: Char-Latin3
    gitpan-cpan-version:      0.97
    gitpan-cpan-path:         INA/Char/Latin3/Char-Latin3-0.97.tar.gz
    gitpan-cpan-author:       INA
    gitpan-cpan-maturity:     released

--- Char/Elatin3.pm
+++ Char/Elatin3.pm
@@ -3,7 +3,7 @@ package Char::Elatin3;
 #
 # Char::Elatin3 - Run-time routines for Char/Latin3.pm
 #
-# http://search.cpan.org/dist/Char-Char::Latin3/
+# http://search.cpan.org/dist/Char-Latin3/
 #
 # Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013, 2014 INABA Hitoshi <ina@cpan.org>
 ######################################################################
@@ -17,7 +17,7 @@ use 5.00503;    # Galapagos Consensus 1998 for primetools
 # (and so on)
 
 BEGIN { eval q{ use vars qw($VERSION) } }
-$VERSION = sprintf '%d.%02d', q$Revision: 0.96 $ =~ /(\d+)/xmsg;
+$VERSION = sprintf '%d.%02d', q$Revision: 0.97 $ =~ /(\d+)/xmsg;
 
 BEGIN {
     if ($^X =~ / jperl /oxmsi) {
